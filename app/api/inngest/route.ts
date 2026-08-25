d453c32cfec84969bc66a6bf3940d09bd7060eec
Peeyush Tiwari <piyushkumarqu888@gmail.com>
2026-08-26 00:09:37 +0530

    Fix Inngest caching issue

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -4,6 +4,7 @@ import { serve } from "inngest/next";
 import { processTask } from "./function";
 import { syncRepoCodebaseFunction, unsyncRepoCodebaseFunction } from "@/features/repo-sync/server/repo-sync-function";
 
+export const dynamic = "force-dynamic";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
